65f3e6c05f3812da29f6dbc7698404d4b5d94126
Thyago T Dissenha <thyago.dissenha@gmail.com>
2022-05-22 19:17:41 -0300

    Java OO: introdução a orienteção a objeto Capitulo 05

---
+++ bytebank-composto/src/TesteContaNegativo.java
@@ -0,0 +1,10 @@
+
+public class TesteContaNegativo {
+	public static void main(String[] args) {
+		Conta conta = new Conta();
+		conta.deposita(100);		
+		System.out.println(conta.saca(200));
+		System.out.println(conta.getSaldo());
+
+	}
+}
