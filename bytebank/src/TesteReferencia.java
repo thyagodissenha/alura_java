609cb87458865cd5a940f03c01ca13970c8b76c5
Thyago T Dissenha <thyago.dissenha@gmail.com>
2022-05-22 16:52:52 -0300

    Java OO: introdução a orienteção a objeto Aula 02

---
+++ bytebank/src/TesteReferencia.java
@@ -0,0 +1,13 @@
+
+public class TesteReferencia {
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 300;
+		
+		System.out.println("saldo da primeira: " + primeiraConta.saldo);
+		
+		Conta segundaConta = primeiraConta;
+		
+		System.out.println(segundaConta.saldo);
+	}
+}
---
+++ sintaxe-variaveis-e-fluxo/src/TestaLaco2.java
@@ -0,0 +1,11 @@
+
+public class TestaLaco2 {
+	public static void main(String[] args) {
+		for(int multiplicador = 0; multiplicador <= 10; multiplicador++) {
+			for(int contador = 0; contador < multiplicador; contador++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
