4218b55bf797eed1ab26d3d173fa65a703aed5f5
Thyago T Dissenha <thyago.dissenha@gmail.com>
2022-05-24 22:58:08 -0300

    Java Poliformismo Capitulo 03

---
+++ bytebank-herdado/src/TesteReferencias.java
@@ -0,0 +1,30 @@
+
+public class TesteReferencias {
+
+	public static void main(String[] args) {
+
+		Gerente g1 = new Gerente();
+		g1.setNome("Marcos");
+		g1.setSalario(5000.0);
+		String nome = g1.getNome();
+		
+		Funcionario f = new Funcionario();
+		f.setSalario(2000.0);
+		
+		EditorVideo ev = new EditorVideo();
+		ev.setSalario(2500.0);
+		
+		Designer d = new Designer();
+		d.setSalario(2000);
+		
+		ControleBonificacao controle = new ControleBonificacao();
+		controle.registra(g1);
+		controle.registra(f);
+		controle.registra(ev);
+		controle.registra(d);
+		
+		System.out.println(controle.getSoma());
+
+	}
+
+}
