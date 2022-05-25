d35aa7fcec8ccea207f2607743abaf09926562d0
Thyago T Dissenha <thyago.dissenha@gmail.com>
2022-05-24 22:12:09 -0300

    Java Poliformismo Capitulo 02

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -12,7 +12,7 @@ public class Gerente extends Funcionario {
 	}
 	
 	public  double getBonificacao() {
-		return this.getSalario() + 1000;
+		return super.getBonificacao() + super.getSalario();
 	}
 
 	public void setSenha(int senha) {
