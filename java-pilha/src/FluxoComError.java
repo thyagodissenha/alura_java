7a667d661c8ade5f4110795b8af693acf8d9cdb1
Thyago T Dissenha <thyago.dissenha@gmail.com>
2022-05-31 09:25:25 -0300

    Java Exceções Capitulo 04

--- java-pilha/src/FluxoComError.java
+++ java-pilha/src/FluxoComError.java
@@ -1,5 +1,5 @@
 
-public class Fluxo {
+public class FluxoComError {
 	
     public static void main(String[] args) {
         System.out.println("Ini do main");
@@ -12,15 +12,15 @@ public class Fluxo {
         System.out.println("Fim do main");
     }
 
-    private static void metodo1() throws MinhaExcecao {
+    private static void metodo1() {
         System.out.println("Ini do metodo1");
         metodo2();
         System.out.println("Fim do metodo1");
     }
 
-    private static void metodo2() throws MinhaExcecao {
+    private static void metodo2() {
         System.out.println("Ini do metodo2");
-        throw new MinhaExcecao("deu muito errado");
-        //System.out.println("Fim do metodo2");
+        metodo2();
+        System.out.println("Fim do metodo2");
     }
 }
